84d6525c1df9a62da0b7322f0a090fb76b3e959e
hassankhan2608 <hassankhan2608@gmail.com>
2024-02-04 19:48:24 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT= '-e.'
+
+def get_requirements(file_path: str) -> List[str]:
+    """
+    This function will return the list of requirements
+    """
+    requirements = []
+    with open(file_path) as file_obj:
+        lines = file_obj.readlines()
+        for line in lines:
+            # Split each line into individual requirements
+            requirements.extend(line.strip().split())
+            
+    # Remove the '-e .' entry if present
+    if HYPEN_E_DOT in requirements:
+        requirements.remove(HYPEN_E_DOT)
+        
+    return requirements
+
+        
+        
+setup(
+    name='m1project',
+    version='0.0.1',
+    author='Mohd Hassan Khan',
+    author_email='hassankhan2608@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
